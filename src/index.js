f2968bb222112e7ad2da8b794579674f17673861
fahri özmen <fahri@veriyaz.com.tr>
2019-07-06 14:22:51 +0300

    must have input file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-i, --inputfile', 'Input CSV File')
+  .parse(process.argv);
+
+if (!program.inputfile)
+console.error(new Error("You have to specify a CSV file to read books."))
+
+ 
