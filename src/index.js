119de0cbf3582a9c4dd5da17ef3aaa24a625b59f
fahri özmen <fahri@veriyaz.com.tr>
2019-07-06 14:29:58 +0300

    formatting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-var program = require('commander');
+const program = require('commander');
 
 program
-  .version('0.1.0')
-  .option('-i, --inputfile', 'Input CSV File')
-  .parse(process.argv);
+    .version('0.1.0')
+    .option('-i, --inputfile', 'Input CSV File')
+    .parse(process.argv);
 
 if (!program.inputfile)
-console.error(new Error("You have to specify a CSV file to read books."))
-
- 
+    console.error(new Error("You have to specify a CSV file to read books."))
